8d76b8ad2d41076d4511ea9f68aca3e9a3cc4f51
Beza <bezawit.teferi@gmail.com>
2017-12-20 20:01:46 -0500

    Radio button grabbing only one clicked value :)

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,16 @@
 //when the user clicks the start button swich page and bring in logic
 // GEN vARS
-
+var unanswered_value_Q1=-1;
+var unanswered_value_Q2=-1;
+var unanswered_value_Q3=-1;
 var correct_a=0;
 var incorrect_answers=0;
-var questions1='what color is the sky';
-var question2='Your fev food';
+var unanswered=0;
+// BRING THIS BACK LATER?
+var questions1='what color is the sky?';
+var question2='Your fev food?';
 var qustion3="What sport do you like?"
+
 var timeout_counter=$('<p> Time Remaining:'+ my_Timer + '</p>'); // this will be used for time count down
 
 //====================Vars holding my button
@@ -13,26 +18,29 @@ var Ql_Answers=$("<p>").append($('<input class="form-check-input q1" type="radio
                       .append($('<input class="form-check-input q1" type="radio" name="list1" value="Blue"> Blue </input>'))
                       .append($('<input class="form-check-input q1" type="radio" name="list1" value="Green"> Green </input>'));
 //==========================================================
-var Q2_Answers=$("<p>").append($('<input class="form-check-input q1" type="radio" name="list2" value="Pizza"> Pizza </input>'))
-                      .append($('<input class="form-check-input q1" type="radio" name="list2" value="Cake"> Cake </input>'))
-                      .append($('<input class="form-check-input q1" type="radio" name="list2" value="Water"> Water </input>'));
+var Q2_Answers=$("<p>").append($('<input class="form-check-input q2" type="radio" name="list2" value="Pizza"> Pizza </input>'))
+                      .append($('<input class="form-check-input q2" type="radio" name="list2" value="Cake"> Cake </input>'))
+                      .append($('<input class="form-check-input q2" type="radio" name="list2" value="Water"> Water </input>'));
 //===================================================================
-var Q3_Answers=$("<p>").append($('<input class="form-check-input q1" type="radio" name="list3"  value="Soccer"> Soccer </input>'))
-                      .append($('<input class="form-check-input q1" type="radio" name="list3"  value="BasketBall"> BasketBall </input>'))
-                      .append($('<input class="form-check-input q1" type="radio" name="list3" value="Football"> Football </input>'));
+var Q3_Answers=$("<p>").append($('<input class="form-check-input q3" type="radio" name="list3"  value="Soccer"> Soccer </input>'))
+                      .append($('<input class="form-check-input q3" type="radio" name="list3"  value="BasketBall"> BasketBall </input>'))
+                      .append($('<input class="form-check-input q3" type="radio" name="list3" value="Football"> Football </input>'));
 
+
+// *********************q1 above is the class i have created for my buttons**************
 // ================================================//
 // create a submite button
 
-var submiteButton=$('<button class="btn btn-primary Submite"> Submite </button>');
+var submiteButton=$("<button class='btn btn-primary Submite'> Submite </button>");
 
-var my_Timer =30;
-
+var my_Timer =5;
+// user is given 30 secs before the page timesout
 //====================================================================
 
 
-
+// the startgame is going to be passed along to the onclick method
 $('.StartButton').on('click',start_Game);
+
 
 
 function start_Game(){
@@ -43,11 +51,13 @@ function start_Game(){
   Counter_Id = setInterval(countdown, 1000);
 
   function countdown(){
-    for (i=0; i < my_Timer; i++){
-      my_Timer--
-      var my_newTime = my_Timer;
-      $(".countdown").append(my_newTime);
-    };
+    if(my_Timer>0){
+      my_Timer--;
+      $(".countdown").text(my_Timer);
+}else{
+
+
+}
 
     // alert(my_newTime);
     // if else
@@ -70,27 +80,92 @@ $(".start_Game").append(submiteButton);
 
 //submit button fires the submite button function =====
 $('.Submite').on('click',Submit_Button);
-//================================Submite function =============//
+//================================Submite function  =============//
 function Submit_Button (){
   // alert("try")
-$(".start_Game").html("<p>Correct Answers:</p>" + correct_a + "<p> Incorrect Answer: </p>"+ incorrect_answers );
+
+  //=***********************if else per question srtarts
+if(unanswered_value_Q1==1){
+correct_a=correct_a+1;
+}else if (unanswered_value_Q1==0) {
+  incorrect_answers=incorrect_answers+1;
+}else{
+  unanswered=unanswered+1;
+};
+
+// if else for q2
+if(unanswered_value_Q2==1){
+correct_a=correct_a+1;
+}else if (unanswered_value_Q2==0) {
+  incorrect_answers=incorrect_answers+1;
+}else{
+  unanswered=unanswered+1;
+};
+
+
+// if else for Q3 
+
+if(unanswered_value_Q3==1){
+correct_a=correct_a+1;
+}else if (unanswered_value_Q3==0) {
+  incorrect_answers=incorrect_answers+1;
+}else{
+  unanswered=unanswered+1;
+};
+
+// *******************************************************if else for Qs ends here
+
+
+$(".start_Game").html("<p>Correct Answers:</p>" + correct_a + "<p> Incorrect Answer: </p>"+ incorrect_answers + "<p> unanswered </p>" + unanswered);
 
 };
 
 // ==============================create a function that compares your answers
-$('.q1').on('click',Clicked_Answ ); // calling on the below function
+$('.q1').on('click',Clicked_Answ_forQ1 ); // calling on the below function
+$('.q2').on('click',Clicked_Answ_forQ2 );
+$('.q3').on('click',Clicked_Answ_forQ3 );
 
+// function for Q1
+function Clicked_Answ_forQ1 (){
 
-function Clicked_Answ (){
 
 var users_pick=($(this).val()); // this here is what the user is clicking
+console.log(this);
+if (users_pick==="Red"){
+    unanswered_value_Q1=1;
+}else{
+  unanswered_value_Q1=0;
+};
+console.log(unanswered_value_Q1);
+};
+// } is for the if statment FOR Q2
+
+function Clicked_Answ_forQ2 (){
+  // it needs to dec in count
+var users_pick=($(this).val()); // this here is what the user is clicking
 // here is my actual winning ans.
-if (users_pick==="Red"|| users_pick==="Pizza"|| users_pick==="Football"){
-correct_a++;
-} else{
-console.log(Incorrect_Answers++);
+if (users_pick==="Pizza"){
+unanswered_value_Q2=1;
+}else{
+  unanswered_value_Q2=0;
+};
+console.log(unanswered_value_Q2);
 };
 
+
+// function 3 for q3
+function Clicked_Answ_forQ3 (){
+   // it needs to dec in count
+var users_pick=($(this).val()); // this here is what the user is clicking
+
+// here is my actual winning ans.
+if (users_pick==="Football"){
+unanswered_value_Q3=1;
+
+}else{
+  unanswered_value_Q3=0;
+};
+console.log(unanswered_value_Q3);
 };
 
 
